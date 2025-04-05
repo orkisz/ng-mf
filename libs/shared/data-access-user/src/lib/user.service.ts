86ca506e7200bdb1b89fc1ced505e6ff359f1482
Krzysztof Orkisz <krzysiek.orkisz@gmail.com>
2025-04-05 22:45:19 +0200

    service ready

--- libs/shared/data-access-user/src/lib/user.service.ts
+++ libs/shared/data-access-user/src/lib/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class UserService {
+  private isUserLoggedIn = new BehaviorSubject(false);
+  isUserLoggedIn$ = this.isUserLoggedIn.asObservable();
 
-  constructor() { }
+  checkCredentials(username: string, password: string) {
+    if (username === 'demo' && password === 'demo') {
+      this.isUserLoggedIn.next(true);
+    }
+  }
+
+  logout() {
+    this.isUserLoggedIn.next(false);
+  }
 }
