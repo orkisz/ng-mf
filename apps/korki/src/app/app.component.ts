cd4f3ccc9a4efdb88c535f0aed2c0552c4266ff7
Krzysztof Orkisz <krzysiek.orkisz@gmail.com>
2025-04-06 09:35:38 +0200

    make login a part of korki

--- apps/korki/src/app/app.component.ts
+++ apps/korki/src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { UserService } from '@ng-mf/data-access-user';
+import { distinctUntilChanged } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [CommonModule, RouterModule],
   selector: 'kimf-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'korki';
+export class AppComponent implements OnInit {
+  private router = inject(Router);
+  private userService = inject(UserService);
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+
+  ngOnInit() {
+    this.isLoggedIn$
+      .pipe(distinctUntilChanged())
+      .subscribe(async (loggedIn) => {
+        // Queue the navigation after initialNavigation blocking is completed
+        setTimeout(() => {
+          if (!loggedIn) {
+            this.router.navigateByUrl('login');
+          } else {
+            this.router.navigateByUrl('');
+          }
+        });
+      });
+  }
 }
