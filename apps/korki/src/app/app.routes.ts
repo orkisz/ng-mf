55b50fa7cf3860b5ddc8d8e2f1af5f23600f6501
Krzysztof Orkisz <krzysiek.orkisz@gmail.com>
2025-04-06 09:53:08 +0200

    dynamic remote

--- apps/korki/src/app/app.routes.ts
+++ apps/korki/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Route } from '@angular/router';
 import { AppComponent } from './app.component';
+import { loadRemoteModule } from '@nx/angular/mf';
 
 export const appRoutes: Route[] = [
   {
     path: 'login',
-    loadChildren: () => import('login/Routes').then((m) => m!.remoteRoutes),
+    loadChildren: () => loadRemoteModule('login', './Routes').then((m) => m!.remoteRoutes),
   },
   {
     path: '',
