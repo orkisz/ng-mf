6b9ee0589b1250544d222442bf41ccc65e8d754f
Krzysztof Orkisz <krzysiek.orkisz@gmail.com>
2025-04-07 08:22:33 +0200

    add full docker & PWA capabilities

--- apps/korki/src/app/app.config.ts
+++ apps/korki/src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    importProvidersFrom(
+      ServiceWorkerModule.register('/ngsw-worker.js', { enabled: !isDevMode() })
+    )
   ],
 };
